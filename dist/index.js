3c15c0398476142aea5ff6a05302a8ad29f2b3d0
ongamele <ogebhuza@gmail.com>
2022-04-07 21:46:43 +0200

    start again

--- dist/index.js
+++ dist/index.js
@@ -17,25 +17,6 @@ const express_graphql_1 = require("express-graphql");
 const Schema_1 = require("./Schema");
 const cors_1 = __importDefault(require("cors"));
 const typeorm_1 = require("typeorm");
-/*import { Statistics } from './Entities/Statistics';
-import { Constants } from './Entities/Constants';
-import { Pump5 } from './Entities/Pump5';
-import { PA1 } from './Entities/PA1';
-import { PA2 } from './Entities/PA2';
-import { PA3 } from './Entities/PA3';
-import { PA4 } from './Entities/PA4';
-import { PA5 } from './Entities/PA5';
-import { PA6 } from './Entities/PA6';
-import { PA7 } from './Entities/PA7';
-import { PA8 } from './Entities/PA8';
-import { PA9 } from './Entities/PA9';
-import { PA10 } from './Entities/PA10';
-import { PA11 } from './Entities/PA11';
-import { PA12 } from './Entities/PA12';
-import { PA13 } from './Entities/PA13';
-import { PA14 } from './Entities/PA14';
-import { PA15 } from './Entities/PA15';
-import { General } from './Entities/General';*/
 //Time Table Entities
 const Attendant1_1 = require("./ShiftsEntities/Attendant1");
 const Attendant2_1 = require("./ShiftsEntities/Attendant2");
@@ -56,37 +37,6 @@ const Attendant16_1 = require("./ShiftsEntities/Attendant16");
 const Attendants_1 = require("./Entities/Attendants");
 const Shifts_1 = require("./Entities/Shifts");
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
-    /*await createConnection({
-      type: 'mysql',
-      host: 'dbfpms.cxcxvgzikyks.us-east-2.rds.amazonaws.com',
-      port: 3306,
-      database: 'sys',
-      username: 'pcsglobalFPMSDB',
-      password: 'pcsg1234',
-      // logging: true,
-      synchronize: false,
-      entities: [
-        Statistics,
-        Constants,
-        Pump5,
-        PA1,
-        PA2,
-        PA3,
-        PA4,
-        PA5,
-        PA6,
-        PA7,
-        PA8,
-        PA9,
-        PA10,
-        PA11,
-        PA12,
-        PA13,
-        PA14,
-        PA15,
-        General,
-      ],
-    });*/
     yield (0, typeorm_1.createConnection)({
         type: 'mysql',
         host: 'dbfpms.cxcxvgzikyks.us-east-2.rds.amazonaws.com',
--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,6 @@ import { graphqlHTTP } from 'express-graphql';
 import { schema } from './Schema';
 import cors from 'cors';
 import { createConnection } from 'typeorm';
-/*import { Statistics } from './Entities/Statistics';
-import { Constants } from './Entities/Constants';
-import { Pump5 } from './Entities/Pump5';
-import { PA1 } from './Entities/PA1';
-import { PA2 } from './Entities/PA2';
-import { PA3 } from './Entities/PA3';
-import { PA4 } from './Entities/PA4';
-import { PA5 } from './Entities/PA5';
-import { PA6 } from './Entities/PA6';
-import { PA7 } from './Entities/PA7';
-import { PA8 } from './Entities/PA8';
-import { PA9 } from './Entities/PA9';
-import { PA10 } from './Entities/PA10';
-import { PA11 } from './Entities/PA11';
-import { PA12 } from './Entities/PA12';
-import { PA13 } from './Entities/PA13';
-import { PA14 } from './Entities/PA14';
-import { PA15 } from './Entities/PA15';
-import { General } from './Entities/General';*/
 
 //Time Table Entities
 import { Attendant1 } from './ShiftsEntities/Attendant1';
@@ -44,38 +25,6 @@ import { Attendants } from './Entities/Attendants';
 import { Shifts } from './Entities/Shifts';
 
 const main = async () => {
-  /*await createConnection({
-    type: 'mysql',
-    host: 'dbfpms.cxcxvgzikyks.us-east-2.rds.amazonaws.com',
-    port: 3306,
-    database: 'sys',
-    username: 'pcsglobalFPMSDB',
-    password: 'pcsg1234',
-    // logging: true,
-    synchronize: false,
-    entities: [
-      Statistics,
-      Constants,
-      Pump5,
-      PA1,
-      PA2,
-      PA3,
-      PA4,
-      PA5,
-      PA6,
-      PA7,
-      PA8,
-      PA9,
-      PA10,
-      PA11,
-      PA12,
-      PA13,
-      PA14,
-      PA15,
-      General,
-    ],
-  });*/
-
   await createConnection({
     type: 'mysql',
     host: 'dbfpms.cxcxvgzikyks.us-east-2.rds.amazonaws.com',

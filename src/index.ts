51940a160a71cbc24ef7250126777b12faeac9d8
ongamele <ogebhuza@gmail.com>
2022-05-16 16:27:19 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ const main = async () => {
     synchronize: false,
     insecureAuth: true,
     entities: [
-       Employees,
+      Employees,
       Shifts,
       MonthRange,
       WetstockMonthRange,
